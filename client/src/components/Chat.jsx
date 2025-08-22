126ead4342707d0591b6bd4ac17a99dc915b7ca4
Deepika Rajput <149066240+Deepika-rajput@users.noreply.github.com>
2025-08-22 23:46:34 +0530

    Update Chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // Replace with your server address
+const socket = io("https://chat-app-backend-we25.onrender.com");
+// Replace with your server address
 
 function Chat() {
   const [messages, setMessages] = useState([]);
